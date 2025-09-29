8c78873b321a425e704b47a6397325ca578c17f7
Aliifah Felda <Aliifah Felda@Aliifah-Felda>
2025-09-29 14:26:40 +0700

    prakalpro pekan 4 kelas A

---
+++ prakalpro_2025_A_2511531011/src/pekan4/multiIf.java
@@ -0,0 +1,31 @@
+package pekan4;
+
+import java.util.Scanner;
+
+public class multiIf {
+
+	public static void main(String[] args) {
+		int umur;
+		char sim;
+		Scanner a = new Scanner(System.in);
+		System.out.println("Input umur anda : ");
+		umur = a.nextInt();
+		System.out.println("Apakah anda sudah punya sim C : ");
+		sim = a.next().charAt(0);
+		a.close();
+		if ((umur>=17)&&(sim=='y')) {
+			System.out.println("Anda sudah dewasa dan boleh bawa motor");
+		}
+		if ((umur>=17)&&(sim!='y')) {
+			System.out.println("Anda sudah dewasa tetapi tidak oleh bawa motor");
+		}
+		if ((umur<17)&&(sim!='y')) {
+			System.out.println("Anda belum cukup umur bawa motor");
+		}
+		if ((umur<17)&&(sim=='y')) {
+			System.out.println("Anda belum cukup umur punya SIM");
+		}
+
+	}
+
+}
